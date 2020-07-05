d22f5f5138ff3a951381b9c7fd94502b74096bd4
Angelica Rodriguez <ing.angelicarodriguez@gmail.com>
2020-07-05 19:11:55 -0400

    Create StorageEntity project

---
+++ Storage-Backend/StorageEntities/Storage/Category.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace StorageEntities.Storage
+{
+    public class Category
+    {
+        public int idcategory { get; set;}
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "No shorter than 3 characters longer than 50 characters.")]
+        public string name { get; set; }
+        [StringLength(256)]
+        public string description { get; set; }
+        public bool condition { get; set; }
+    }
+}

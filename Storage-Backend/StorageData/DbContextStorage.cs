dc331091e66f58a2e840b22a3fdde9faa93c7e8c
Angelica Rodriguez <ing.angelicarodriguez@gmail.com>
2020-07-05 19:11:00 -0400

    Create StorageData Project

---
+++ Storage-Backend/StorageData/DbContextStorage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StorageData.Mapping.Storage;
+using StorageEntities.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageData
+{
+    public class DbContextStorage : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+       
+        public DbContextStorage(DbContextOptions<DbContextStorage> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            
+        }
+    }
+}
---
+++ Storage-Backend/StorageData/Mapping/Storage/CategoryMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StorageEntities.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StorageData.Mapping.Storage
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("category")
+                .HasKey(c => c.idcategory);
+            builder.Property(c => c.name)
+                .HasMaxLength(50);
+            builder.Property(c => c.description)
+                .HasMaxLength(256);
+        }
+    }
+}
